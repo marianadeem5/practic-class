222c9fcb8e46e777b35223000bd0efd3f9b5b560
marianadeem5 <mn0041798@gmail.com>
2017-03-19 08:27:19 -0700

    Create student.java

---
+++ student.java
@@ -0,0 +1,14 @@
+import java.util.scanner;
+public class Student
+{
+public static void(string[]args)
+{
+int rollno;
+int marks;
+scanner scane=new scan(system.in);
+rollno=scan.nextInt();
+scanner scan =new scan(system.in);
+marks=scan.nextInt();
+system.out.print("student roll no is="+rollno()+"student marks is="+marks());
+}
+}
